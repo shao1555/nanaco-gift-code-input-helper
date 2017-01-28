fb7d6cdd3db89df21b30705d9bed04673d06a8af
shao <shao@1555.info>
2017-01-28 22:16:24 +0900

    準備できた

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,17 +1,20 @@
 'use strict';
+document.addEventListener("DOMContentLoaded", function(){
+  const firstInput = document.querySelector('form#registerForm[action="/ap/p/register2.do"] input#gift01');
+  if (firstInput) {
+    firstInput.setAttribute('maxlength', '20');
+    firstInput.addEventListener('keyup', function(){
+      const str = firstInput.value;
+      let matches;
+      if (matches = /^(\w{4})\-?(\w{4})\-?(\w{4})\-?(\w{4})$/.exec(str)) {
+        matches.shift();
+        document.querySelector("#gift01").value = matches[0];
+        document.querySelector("#gift02").value = matches[1];
+        document.querySelector("#gift03").value = matches[2];
+        document.querySelector("#gift04").value = matches[3];
+        document.querySelector("#gift04").focus();
+      }
+    });
+  }
 
-firstInput = document.querySelector('form#registerForm[action="/ap/p/register2.do"] input#gift01');
-if (firstInput && typeof window.change1 == 'function') {
-  firstInput.setAttribute('maxlength', '20');
-  firstInput.removeEventListener('keyup', window.change1);
-  firstInput.addEventListener('keyup', function(){
-    var str = document.getElementById("gift01").value;
-    if (matches = /^(\w{4})\-?(\w{4})\-?(\w{4})\-?(\w{4})$/.exec(str)) {
-      matches.shift();
-      document.getElementById("gift01").value = matches[0];
-      document.getElementById("gift02").value = matches[1];
-      document.getElementById("gift03").value = matches[2];
-      document.getElementById("gift04").value = matches[3];
-    }  
-  });
-}
+});
