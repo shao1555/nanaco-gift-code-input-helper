dcabd96c47fdf2db15a94eaef2b13009eaf1ede5
shao <shao@1555.info>
2017-01-28 23:13:48 +0900

    1.0.2 - auto submit

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,10 +1,11 @@
 'use strict';
 document.addEventListener("DOMContentLoaded", function(){
-  const firstInput = document.querySelector('form#registerForm[action="/ap/p/register2.do"] input#gift01');
-  if (firstInput) {
-    firstInput.setAttribute('maxlength', '20');
-    firstInput.addEventListener('keyup', function(){
-      const str = firstInput.value;
+  // コード入力画面
+  const codeInput = document.querySelector('form#registerForm[action="/ap/p/register2.do"] input#gift01');
+  if (codeInput) {
+    codeInput.setAttribute('maxlength', '20');
+    codeInput.addEventListener('keyup', function(){
+      const str = codeInput.value;
       let matches;
       if (matches = /^(\w{4})\-?(\w{4})\-?(\w{4})\-?(\w{4})$/.exec(str)) {
         matches.shift();
@@ -12,10 +13,15 @@ document.addEventListener("DOMContentLoaded", function(){
         document.querySelector("#gift02").value = matches[1];
         document.querySelector("#gift03").value = matches[2];
         document.querySelector("#gift04").value = matches[3];
-        document.querySelector("#gift04").focus();
+        document.querySelector('form#registerForm[action="/ap/p/register2.do"]').submit();
       }
     });
-    firstInput.focus();
+    codeInput.focus();
   }
 
+  // 登録内容確認画面
+  const confirmationForm = document.querySelector('form#registerForm[action="/ap/p/register4.do"]');
+  if (confirmationForm) {
+    confirmationForm.submit();
+  }
 });
