738e5ba8901cb88630c73872d2e0ea68482f8180
shao <shao@1555.info>
2017-01-29 14:21:06 +0900

    refactor

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,24 +1,25 @@
 'use strict';
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", () => {
   // コード入力画面
-  const codeInput = document.querySelector('form#registerForm[action="/ap/p/register2.do"] input#gift01');
-  if (codeInput) {
-    codeInput.setAttribute('maxlength', '20');
-    codeInput.addEventListener('keyup', function(){
-      const str = codeInput.value;
-      let matches;
-      if (matches = /^(\w{4})\-?(\w{4})\-?(\w{4})\-?(\w{4})$/.exec(str)) {
-        matches.shift();
-        document.querySelector("#gift01").value = matches[0];
-        document.querySelector("#gift02").value = matches[1];
-        document.querySelector("#gift03").value = matches[2];
-        document.querySelector("#gift04").value = matches[3];
-        document.querySelector('form#registerForm[action="/ap/p/register2.do"]').submit();
-      }
-    });
-    // 準備ができたら入力欄の背景色を変える
-    document.querySelectorAll('form#registerForm[action="/ap/p/register2.do"] input[size="4"]').forEach(function(input){input.style.backgroundColor = '#e0ffe0'});
-    codeInput.focus();
+  const codeEntryForm = document.querySelector('form#registerForm[action="/ap/p/register2.do"]')
+  if (codeEntryForm) {
+    let inputForms = codeEntryForm.querySelectorAll('input[size="4"]');
+    if (inputForms.length > 0) {
+      // NodeList から Array へのキャスト
+      inputForms = Array.apply(null, inputForms);
+      inputForms[0].setAttribute('maxlength', '20');
+      inputForms[0].addEventListener('keyup', () => {
+        let matches;
+        if (matches = /(\w{4})\-?(\w{4})\-?(\w{4})\-?(\w{4})/.exec(inputForms[0].value)) {
+          matches.shift();
+          matches.forEach( (match) => { inputForms.shift().value = match; } );
+          codeEntryForm.submit();
+        }
+      });
+      // 準備ができたら入力欄の背景色を変える
+      inputForms.forEach( (input) => { input.style.backgroundColor = '#e0ffe0' } );
+      codeInput.focus();
+    }
   }
 
   // 登録内容確認画面
